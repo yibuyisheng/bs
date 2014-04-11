679839ecbf4d07437e6206eb621a0f475cfd8917
yibuyisheng <yibuyisheng@163.com>
2014-04-11 16:16:33 +0800

    add kindeditor

--- public/javascripts/modules/manage.js
+++ public/javascripts/modules/manage.js
@@ -29,4 +29,18 @@ $(function() {
 			showSuccess('添加成功！');
 		});
 	});
+
+
+	var editor;
+  KindEditor.ready(function(K) {
+    editor = K.create('textarea[name="detail"]', {
+      resizeType : 1,
+      allowPreviewEmoticons : false,
+      allowImageUpload : false,
+      items : [
+        'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold', 'italic', 'underline',
+        'removeformat', '|', 'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist',
+        'insertunorderedlist', '|', 'image']
+    });
+  });
 });
